93b85bdbe609268c9a423f9f4bef2170ceaee729
Alain Bernier <alain@alainbernier.com>
2022-01-29 17:59:26 -0500

    ex-fectch fini

--- index.js
+++ index.js
@@ -1,17 +1,47 @@
-function addImage() {
-    document.getElementById("imgContainer").innerHTML = '<img></img>';
-    fetch('/assets/img1.jpg')
-    .then((response) => {
-        if (!response.ok) {
-            throw new Error('HTTP error, status = ' + response.status);
-        }
-        return response.blob()
-    }).then((blob) => {
-        const myImage = document.querySelector('img');
-        const objectURL = URL.createObjectURL(blob);
-        myImage.src = objectURL;
-    });
-}
+fetch('http://127.0.0.1:5500/assets/movies.json')
+.then(function(response) {
+return response.json();
+})
+.then(function(myJson) {
+console.log(JSON.stringify(myJson));
+
+console.log(myJson.members);
+
+for (let member of myJson.members) { console.log(member.name) }
+
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// --> pour fetch-ex-1 <-- | 
+// function addImage() {
+//     document.getElementById("imgContainer").innerHTML = '<img></img>';
+//     fetch('/assets/img1.jpg')
+//     .then((response) => {
+//         if (!response.ok) {
+//             throw new Error('HTTP error, status = ' + response.status);
+//         }
+//         return response.blob()
+//     }).then((blob) => {
+//         const myImage = document.querySelector('img');
+//         const objectURL = URL.createObjectURL(blob);
+//         myImage.src = objectURL;
+//     });
+// }
 
 
 
