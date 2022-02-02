55cc334fadbdbbd56f86b4a7b4c40ae6c3d98a56
Alain Bernier <alain@alainbernier.com>
2022-02-01 20:17:51 -0500

    modifmodif service workers

--- sw.js
+++ sw.js
@@ -1,31 +1,45 @@
-self.addEventListener('install', function (event) {
-  event.waitUntil(
-    caches.open('v1').then(function (cache) {
-      return cache.addAll([
-        '/pwa-bootstrap/index.html',
-        '/pwa-bootstrap/index.js',
-        '/pwa-bootstrap/bootstrap-5.1.3-dist/css/bootstrap.min.css',
-        '/pwa-bootstrap/bootstrap-5.1.3-dist/js/bootstrap.bundle.min.js',
-        '/pwa-bootstrap/favicon.ico',
-        '/pwa-bootstrap/assets/Boutique.jpeg',
-        '/pwa-bootstrap/assets/GuideEmploye.jpeg',
-        '/pwa-bootstrap/assets/Onboarding.jpeg',
-        '/pwa-bootstrap/icons-1.7.2/font/bootstrap-icons.css',
+self.addEventListener('install', (event) => {
+    event.waitUntil(
+      caches.open('v1').then((cache) => {
+        return cache.addAll([
+        '/index.html',
+        '/index.js',
+        '/bootstrap-5.1.3-dist/css/bootstrap.min.css',
+        '/bootstrap-5.1.3-dist/js/bootstrap.bundle.min.js',
+        '/favicon.ico',
+        '/assets/Boutique.jpeg',
+        '/assets/GuideEmploye.jpeg',
+        '/assets/Onboarding.jpeg',
+        '/icons-1.7.2/font/bootstrap-icons.css',
       ]);
     })
   );
 });
 
-self.addEventListener('fetch', function (event) {
-  console.log('Fetching ...' + event.request.url);
-  event.respondWith(
-    caches.match(event.request).then((response) => {
-      if (response !== undefined) {
-        return response;
-      } else {
-        console.log('Fetching from fetch ...' + event.request.url);
-        return fetch(event.request);
-      }
+self.addEventListener('fetch', (event) => {
+    event.respondWith(
+      caches.match(event.request).then((resp) => {
+        return resp || fetch(event.request).then( async (response) => {
+          return caches.open('v1').then((cache) => {
+            cache.put(event.request, response.clone());
+            return response;
+        });
+        });
     })
-  );
+    );
 });
+
+
+// self.addEventListener('fetch', function (event)  {
+//   console.log('Fetching ...' + event.request.url);
+//   event.respondWith(
+//     caches.match(event.request).then((response) => {
+//       if (response !== undefined) {
+//         return response;
+//       } else {
+//         console.log('Fetching from fetch ...' + event.request.url);
+//         return fetch(event.request);
+//       }
+//     })
+//   );
+// });
