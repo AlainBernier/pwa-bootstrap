924906e9ef5cfdbf8ce806d51616ecd13d074abd
Alain Bernier <alain@alainbernier.com>
2022-02-01 22:28:49 -0500

    retrait fichiers panier blog ex-fetch

--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
 self.addEventListener('install', (event) => {
-    event.waitUntil(
-      caches.open('v1').then((cache) => {
-        return cache.addAll([
+  event.waitUntil(
+    caches.open('v1').then((cache) => {
+      return cache.addAll([
         '/index.html',
         '/index.js',
         '/bootstrap-5.1.3-dist/css/bootstrap.min.css',
@@ -10,6 +10,10 @@ self.addEventListener('install', (event) => {
         '/assets/Boutique.jpeg',
         '/assets/GuideEmploye.jpeg',
         '/assets/Onboarding.jpeg',
+        '/assets/icon-192x192.png',
+        'assets/icon-256x256.png',
+        'assets/icon-384x384.png',
+        'assets/icon-512x512.png',
         '/icons-1.7.2/font/bootstrap-icons.css',
       ]);
     })
@@ -17,18 +21,20 @@ self.addEventListener('install', (event) => {
 });
 
 self.addEventListener('fetch', (event) => {
-    event.respondWith(
-      caches.match(event.request).then((resp) => {
-        return resp || fetch(event.request).then( async (response) => {
+  event.respondWith(
+    caches.match(event.request).then((resp) => {
+      return (
+        resp ||
+        fetch(event.request).then(async (response) => {
           return caches.open('v1').then((cache) => {
             cache.put(event.request, response.clone());
             return response;
-        });
-        });
+          });
+        })
+      );
     })
-    );
+  );
 });
-
 
 // self.addEventListener('fetch', function (event)  {
 //   console.log('Fetching ...' + event.request.url);
