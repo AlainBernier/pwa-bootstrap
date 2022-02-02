eb68f7f1398d807ecf572aa6b5faee8aafbb2f22
Alain Bernier <alain@alainbernier.com>
2022-02-02 12:11:58 -0500

    mise en forme du code

--- sw.js
+++ sw.js
@@ -1,40 +1,42 @@
-this.addEventListener('install', (event) => {
-    event.waitUntil(
-      caches.open('v1').then((cache) => {
-        return cache.addAll([
-        'index.html',
-        'index.js',
-        'bootstrap-5.1.3-dist/css/bootstrap.min.css',
-        'bootstrap-5.1.3-dist/js/bootstrap.bundle.min.js',
-        'favicon.ico',
-        'assets/Boutique.jpeg',
-        'assets/GuideEmploye.jpeg',
-        'assets/Onboarding.jpeg',
-        'assets/icon-192x192.png',
-        'assets/icon-256x256.png',
-        'assets/icon-384x384.png',
-        'assets/icon-512x512.png',
-        'assets/APPCONNECT_32x32.png',
-        'assets/apple-touch-icon.png',
-        'assets/logoApps.svg',
-        'icons-1.7.2/font/bootstrap-icons.css'
-        
+this.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open("v1").then((cache) => {
+      return cache.addAll([
+        "index.html",
+        "index.js",
+        "bootstrap-5.1.3-dist/css/bootstrap.min.css",
+        "bootstrap-5.1.3-dist/js/bootstrap.bundle.min.js",
+        "favicon.ico",
+        "assets/Boutique.jpeg",
+        "assets/GuideEmploye.jpeg",
+        "assets/Onboarding.jpeg",
+        "assets/icon-192x192.png",
+        "assets/icon-256x256.png",
+        "assets/icon-384x384.png",
+        "assets/icon-512x512.png",
+        "assets/APPCONNECT_32x32.png",
+        "assets/apple-touch-icon.png",
+        "assets/logoApps.svg",
+        "icons-1.7.2/font/bootstrap-icons.css",
       ]);
     })
   );
 });
 
-this.addEventListener('fetch', (event) => {
-    event.respondWith(
-      caches.match(event.request).then((resp) => {
-        return resp || fetch(event.request).then( async (response) => {
-          return caches.open('v1').then((cache) => {
+this.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((resp) => {
+      return (
+        resp ||
+        fetch(event.request).then(async (response) => {
+          return caches.open("v1").then((cache) => {
             cache.put(event.request, response.clone());
             return response;
-        });
-        });
+          });
+        })
+      );
     })
-    );
+  );
 });
 // this.addEventListener('activate', (event) => {
 //   console.log(' Activation avec succès des service worker');
